ef1f78093974837efa914b99f1215e42ef466e99
yan7843 <qoo7843@yahoo.com.tw>
2018-01-11 22:14:00 +0800

    move authenticate_user! to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  before_action :authenticate_user!
 
 private
 
@@ -10,5 +10,5 @@ private
       redirect_to root_path
     end
   end
-  
+
 end

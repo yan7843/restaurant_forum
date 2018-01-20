4004855f9a63b1ae7f1e57b2e9283c46fb5d4f8c
yan7843 <qoo7843@yahoo.com.tw>
2018-01-20 16:06:49 +0800

    setup routes for feeds and dashboard pages

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,14 @@ Rails.application.routes.draw do
 
   resources :restaurants, only: [:index, :show] do
     resources :comments, only: [:create, :destroy]
+  
+    collection do
+      get :feeds
+    end
+
+    member do
+      get :dashboard
+    end
   end
 
   resources :users, only: [:show, :edit, :update]

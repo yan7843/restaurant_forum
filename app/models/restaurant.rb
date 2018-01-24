eb6a8b0191d177bf9f155f8e93a3f9e75117f8b9
yan7843 <qoo7843@yahoo.com.tw>
2018-01-24 20:42:16 +0800

    creata favorite model & setup relation among user,restaurant and favorite

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,7 @@ class Restaurant < ApplicationRecord
   mount_uploader :image, PhotoUploader
   belongs_to :category, optional: true
   has_many :comments, dependent: :destroy
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
 end

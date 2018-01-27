aec1074097b2f09bab5e6f25f30958fa03312a08
yan7843 <qoo7843@yahoo.com.tw>
2018-01-27 16:36:11 +0800

    add following? method to user model for checking follow / unfollow button

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,10 +20,12 @@ class User < ApplicationRecord
         
         has_many :followships, dependent: :destroy
         has_many :followings, through: :followships
-        
+
         def admin?
           self.role == "admin"
         end
 
-
+        def following?(user)
+          self.followings.include?(user)
+        end
 end
